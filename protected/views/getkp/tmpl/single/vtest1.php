460025011ed8485ccd387ad8c4840fcca86f0a78
Cheshenkov <cheshenkov@lbr.ru>
2014-01-15 09:59:43 +0300

    Edit provalinskiy test KP

--- protected/views/getkp/tmpl/single/vtest1.php
+++ protected/views/getkp/tmpl/single/vtest1.php
@@ -31,7 +31,7 @@
             <tr><td><img src="http://www.git-lbr.ru/images/kp/10.gif" style="display: block;"/></td></tr>
             <tr>
                 <td>
-                    <span style="font-family: 'Arial Black', Gadget, sans-serif; font-size: 15px; color: rgb(6, 69, 173); font-weight: 900; text-transform: uppercase;">Внимание!</span> <span style="font-family: 'Trebuchet MS', Helvetica, sans-serif; color: rgb(6, 69, 173); font-size: 14px;">ООО «Сеть Агромаркетов», в рамках программы по работе с ключевыми клиентами, предлагает вашему предприятию  приобрести оборудование для почвообработке в рассрочку на следующих условиях:
+                    <span style="font-family: 'Trebuchet MS', Helvetica, sans-serif; color: rgb(6, 69, 173); font-size: 14px;">ООО «Сеть Агромаркетов», в рамках программы по работе с ключевыми клиентами, предлагает вашему предприятию  приобрести оборудование для почвообработки в рассрочку на следующих условиях:
                     </span><span style="font-family: 'Trebuchet MS', Helvetica, sans-serif; font-weight: bold; font-size: 15px; color: rgb(6, 69, 173); "><br>Аванс- 25%.
                     Срок кредита – до 7 месяцев.
                     Промежуточные платежи – не более 10% в месяц.
