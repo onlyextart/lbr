b8c1978fd9c886194dcb6f4ba3ef711fb2d7569d
YURY <vasiliyan@lbr.ru>
2014-01-16 13:34:06 +0300

    TinyMCE in Product News Banners Proizvoditeli

--- protected/modules/administrator/views/banners/manage.php
+++ protected/modules/administrator/views/banners/manage.php
@@ -47,24 +47,28 @@ else{
         <?php echo $form->textField( $imageModel , "[$regionId][$imageNum]bottom_right", array('class'=>'imageBottom_right imageFeature') ); ?>
     </div>
 <?php } ?>
-<script type="text/javascript" src="/js/tinymce/tinymce.min.js"></script>
+<script type="text/javascript" src="/js/tinymce_3_x/tiny_mce.js"></script>
 <script type="text/javascript">
 tinymce.myOptions = {
-    //selector: ".with_tinymce",
-    theme: "modern",
-    plugins: [
-        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-        "searchreplace wordcount visualblocks visualchars code fullscreen",
-        "insertdatetime media nonbreaking save table contextmenu directionality",
-        "emoticons template paste textcolor"
-    ],
-    theme_advanced_resizing : true,
-    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-    toolbar2: "print preview media | forecolor backcolor emoticons",
-    image_advtab: true,
-}
-//tinymce.init(tinymce.myOptions);
+        width: "100%",
+        mode : "textareas",
+        editor_selector: "with_tinymce",
+        theme : "advanced",
+        language : "ru",
+        plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+        relative_urls: false,
+        convert_urls : false,
 
+        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true
+
+}
 </script>
 <h2>
     <?php echo $pageHeader ?>
@@ -278,7 +282,7 @@ tinymce.myOptions = {
         </div>
     </div>
     <div class="regional_features_wrapper">
-        <div class="all_regions_forms">
+        <div class="all_regions_forms">        
             <?php foreach( $regionalBanners as $regionId => $regionalBanner ): ?>
             <div class="regional_features <?php if( !$regionalBanner->isNewRecord || 
                                                     $regionId == Yii::app()->params['defaultRegionId'] ) 
@@ -287,8 +291,15 @@ tinymce.myOptions = {
                     <div class="row ">
                         <?php echo $form->error( $regionalBanner , "[$regionId]name" ); ?>
                         <?php echo $form->labelEx( $regionalBanner , "[$regionId]name" ); ?>
-                        <?php echo $form->textField( $regionalBanner , "[$regionId]name", array('style'=>'width:95%') ); ?>
+                        <?php echo $form->textArea( $regionalBanner , "[$regionId]name", array('class'=>'width = 95%')); ?>
                     </div>
+                </div>
+                
+                
+                <div class="row ">
+                    <?php echo $form->labelEx($regionalBanner,"[$regionId]description"); ?><br />
+                    <?php echo $form->textArea($regionalBanner,"[$regionId]description", array('class'=>'with_tinymce')); ?>
+                    <?php echo $form->error($regionalBanner,"[$regionId]description"); ?>
                 </div>
                 <div class="banner_images_wrapper">
                     <div class="image_blank_form" style="display:none;">
@@ -307,11 +318,7 @@ tinymce.myOptions = {
                         <?php endif; ?>
                     <?php endforeach; ?>
                 </div>
-                <div class="row">
-                    <?php echo $form->error( $regionalBanner , "[$regionId]description" ); ?>
-                    <?php echo $form->labelEx( $regionalBanner , "[$regionId]description" ); ?>
-                    <?php echo $form->textarea( $regionalBanner , "[$regionId]description", array('class'=>'with_tinymce') ); ?>
-                </div>
+                
             </div>
             <?php endforeach; ?>
         </div>
@@ -328,7 +335,7 @@ tinymce.myOptions = {
 
         <div id="tabs">
             <ul>
-            </ul>
+            </ul>            
         </div>
         <div class="manage_buttons buttons">
             <?php echo CHtml::link('Закрыть', '/administrator/banners/', array('class'=>'btn del')); ?>
@@ -516,7 +523,7 @@ tinymce.myOptions = {
             image.append(deleteImageButton);
             image.append(form);
             
-            //Обновить все фтрибуты name всех полей ввода форм изображений
+            //Обновить все атрибуты name всех полей ввода форм изображений
             imagesWrapper.find('.uploaded_file_wrapper').each(function(){
                 self.imageInputNameAttrProcessing( $(this) );
             });
