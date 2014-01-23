9cafd66e4a5867cffc9a7986ab24f6729bc03549
Cheshenkov <cheshenkov@lbr.ru>
2014-01-23 11:30:41 +0300

    Add request finance_block_text

--- protected/views/getkp/finance_block.php
+++ protected/views/getkp/finance_block.php
@@ -2,12 +2,8 @@
             <tr><td><img src="http://www.git-lbr.ru/images/kp/10.gif" style="display: block;"/></td></tr>
             <tr>
                 <td>
-                    <span style="font-family: 'Trebuchet MS', Helvetica, sans-serif; color: rgb(6, 69, 173); font-size: 14px;">ООО «Сеть Агромаркетов», в рамках программы по работе с ключевыми клиентами, предлагает вашему предприятию  приобрести оборудование для почвообработки в рассрочку на следующих условиях:
-                    </span><span style="font-family: 'Trebuchet MS', Helvetica, sans-serif; font-weight: bold; font-size: 15px; color: rgb(6, 69, 173); "><br>Аванс- 25%.
-                    Срок кредита – до 7 месяцев.
-                    Промежуточные платежи – не более 10% в месяц.
-                    % удорожания – 0%.
-                    Дополнительное обеспечение не требуется!
+                    <span style="font-family: 'Trebuchet MS', Helvetica, sans-serif; font-size: 14px; color: rgb(6, 69, 173); "><br>Аванс- 25%.
+                    <?php echo $_REQUEST['finance_block_text'];    ?>
                     </span> 
                 </td>
             </tr>
