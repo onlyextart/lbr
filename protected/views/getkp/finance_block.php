5faabc0e21a1e4ea551fb029346c0e5f6b84a8f1
Cheshenkov <cheshenkov@lbr.ru>
2014-01-23 15:20:50 +0300

    Justify in KP. Finance block

--- protected/views/getkp/finance_block.php
+++ protected/views/getkp/finance_block.php
@@ -1,8 +1,8 @@
-<table width="638" style="border-collapse: collapse; border-bottom: 1px rgb(6, 69, 173) dashed" cellspacing="0" cellpadding="0" align="center" bgcolor="#FFFFFF">
+<table width="638" style="border-collapse: collapse; border-bottom: 1px rgb(6, 69, 173) dashed; text-align: justify;" cellspacing="0" cellpadding="0" align="center" bgcolor="#FFFFFF">
             <tr><td><img src="http://www.git-lbr.ru/images/kp/10.gif" style="display: block;"/></td></tr>
             <tr>
                 <td>
-                    <span style="font-family: 'Trebuchet MS', Helvetica, sans-serif; font-size: 14px; color: rgb(6, 69, 173); ">
+                    <span style="font-family: 'Trebuchet MS', Helvetica, sans-serif; font-size: 14px; color: rgb(6, 69, 173); text-align: justify;">
                     <?php echo $finance_block_text;    ?>
                     </span> 
                 </td>
