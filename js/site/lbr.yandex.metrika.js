0c12314c0f44700be2b5f10044136f47f1d742df
Cheshenkov <cheshenkov@lbr.ru>
2014-01-29 10:35:48 +0300

    Yandex stats edit

--- js/site/lbr.yandex.metrika.js
+++ js/site/lbr.yandex.metrika.js
@@ -1,15 +1,22 @@
-(function(w, d, c) {
+(function(d, w, c) {
     (w[c] = w[c] || []).push(function() {
         try {
-            w.yaCounter13203052 = new Ya.Metrika({id: 13203052, enableAll: true});
-        }
-        catch(e) {}
+            w.yaCounter13203052 = new Ya.Metrika({id:13203052,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,params:window.yaParams||{ }});
+        } catch(e) { }
     });
 
-    var e = d.createElement('script');
-    e.type = 'text/javascript';
-    e.async = true;
-    e.src = (d.location.protocol == 'https:' ? 'https:' : 'http:') + '//mc.yandex.ru/metrika/watch.js';
-    var s = d.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(e, s);
-})(window, document, 'yandex_metrika_callbacks');
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
