9ed9fca5b4a161765d523e12c3f13bfe47d0924b
Cheshenkov <cheshenkov@lbr.ru>
2014-01-29 11:29:37 +0300

    Add yandex stats widget to admin panel

--- protected/modules/administrator/views/default/index.php
+++ protected/modules/administrator/views/default/index.php
@@ -1,17 +1,33 @@
-<?php
-/* @var $this DefaultController */
-
-$this->breadcrumbs=array(
-	$this->module->id,
-);
-?>
-<h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
-
-<p>
-This is the view content for action "<?php echo $this->action->id; ?>".
-The action belongs to the controller "<?php echo get_class($this); ?>"
-in the "<?php echo $this->module->id; ?>" module.
-</p>
-<p>
-You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
-</p>
+<div class="yandex-info">
+    <label>Статистика Yandex за сегодня</label>
+    <div id="show">
+        <span class="label">Просмотры:</span>
+        <span class="count"><img src="/images/loading.gif"/></span>
+    </div>
+    <div id="visit">
+        <span class="label">Визиты:</span>
+        <span class="count"><img src="/images/loading.gif"/></span>
+    </div>
+    <div id="unique">
+        <span class="label">Уникальные посетители:</span>
+        <span class="count"><img src="/images/loading.gif"/></span>
+    </div>
+</div>
+<style>
+    .yandex-info label{
+        font-size: 14px;
+        font-weight: bold;
+    }
+</style>
+<script>
+    $(document).ready(function(){
+        $.getJSON('http://bs.yandex.ru/informer/13203052/json?callback=?', function(data) {
+            if (data) {
+                $('#show .count').html(data.pageviews[0]);
+                $('#visit .count').html(data.visits[0]);
+                $('#unique .count').html(data.uniques[0]);
+            }
+        });   
+    });
+     
+</script>
