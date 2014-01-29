5c6782d8daa375d9aa52e0ecab2e79ebcc35b00f
Cheshenkov <cheshenkov@lbr.ru>
2014-01-29 10:29:17 +0300

    Yandex and Google stats to script file

---
+++ js/site/lbr.yandex.metrika.js
@@ -0,0 +1,15 @@
+(function(w, d, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter13203052 = new Ya.Metrika({id: 13203052, enableAll: true});
+        }
+        catch(e) {}
+    });
+
+    var e = d.createElement('script');
+    e.type = 'text/javascript';
+    e.async = true;
+    e.src = (d.location.protocol == 'https:' ? 'https:' : 'http:') + '//mc.yandex.ru/metrika/watch.js';
+    var s = d.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(e, s);
+})(window, document, 'yandex_metrika_callbacks');
