41b84f617659985d46b35afdf751d4feab3183da
YURY <vasiliyan@mns-notebook143.lbr.ru>
2014-03-05 11:44:42 +0300

    Ispravlenie cen

--- protected/views/getkp/index.php
+++ protected/views/getkp/index.php
@@ -1,53 +1,3 @@
-<?
-$print = '';
-if(is_object($data))
-{
-    $array = array();
-    $mas1 = false;
-    foreach ($data as $prop=>$val)
-    {
-        if(is_array($val))
-        {
-            foreach ($val as $i=>$price)
-            {
-                if ($mas1 == false){
-                    
-                    $array['price'.($i+1)] = $price;
-                                      
-                }
-                else {
-                    $array['price_blue'.($i+1)] = $price;
-                    
-                }
-                
-            }
-            $mas1 = true;
-        }else{
-            $array[$prop]=$val;
-        }
-    }
-   
-    $array['user_info'] = $array['user'];
-        
-    if(in_array($data->temp_id, $unique))
-         Yii::app()->getController()->renderPartial('tmpl/single/'.$data->temp_id.'p1', array('data'=>$array, 'filial'=>$filial, 'template'=>$template));   
-    else
-         Yii::app()->getController()->renderPartial('tmpl/single/one-print', array('data'=>$array, 'filial'=>$filial, 'template'=>$template));   
-}
-else{
-    $suf = $suf2 = '';
-    if($data['print']==1){
-        $suf = 'p1';
-        $suf2 = '-print';
-    }
-    if(in_array($data['temp_id'], $unique))
-        Yii::app()->getController()->renderPartial('tmpl/single/'.$data['temp_id'].$suf, array('data'=>$data, 'filial'=>$filial, 'template'=>$template));    
-    else
-        Yii::app()->getController()->renderPartial('tmpl/single/one'.$suf2, array('data'=>$data, 'filial'=>$filial, 'template'=>$template));
-}
-  
-?>
-
 <?php
 $print = '';
 if(is_object($data))
@@ -95,4 +45,3 @@ else{
     else
         Yii::app()->getController()->renderPartial('tmpl/single/one'.$suf2, array('data'=>$data, 'filial'=>$filial, 'template'=>$template));
 }
-
