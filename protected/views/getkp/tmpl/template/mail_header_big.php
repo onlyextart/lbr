6cec20d94e2e949f23f574ef4269d3a3bbf1bb3f
YURY <vasiliyan@lbr.ru>
2014-07-11 12:02:12 +0300

    edit mail_header_big

--- protected/views/getkp/tmpl/template/mail_header_big.php
+++ protected/views/getkp/tmpl/template/mail_header_big.php
@@ -1,22 +1,5 @@
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
-    <style>
-        p{mso-line-height-rule:exactly !important; line-height: 13pt;}
-        a, a:active, a:visited{color:black;}
-    </style>
-</head>
-<body style="background: #eeeeee; margin: 0; padding: 0; border: 0;">
-    <style>
-        p{mso-line-height-rule: exactly !important; line-height: 13pt;}
-        a, a:active, a:visited{color:black;}
-    </style>
 
-    <!-- Background grey -->
-    <table style="background: #eeeeee; margin: 0; padding: 0; border: 0; width: 100%;">
-        <tr>
-            <td>
-                <!-- Background grey -->
+<!-- Background grey -->
 
                 <table width="800" bgcolor="#eeeeee" cellspacing="0" cellpadding="0" align="center" style="border: 0; border-collapse: collapse;">
                     <tr><td style="padding-top: 15px;"><img src="http://www.lbr.ru/images/kp/default/mail-top-line.jpg" width="800" height="10" style="border: 0; float: left;" alt="Логотип ЛБР-Агромаркет"></td></tr>
