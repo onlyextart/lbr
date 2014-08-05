98e236cb8eab02f4595f77b71d20319f496739a4
Tatsyana Krylova <krilova@lbr.ru>
2014-08-05 16:35:30 +0300

    mightiness view

--- protected/views/mightiness/index.php
+++ protected/views/mightiness/index.php
@@ -1,37 +1,37 @@
 <?php $this->widget('ext.mightinessWidget.MightinessWidget'); ?>
 <div id="mightiness-results">
     <div class="one_banner">
-        <h3 val="80-120" label="для 80 л.с.">ДЛЯ 80 ЛОШАДИНЫХ СИЛ</h3>
+        <h3 val="80-120" label="для 80 л.с.">для тракторов 80 л.с.</h3>
         <div class="m_image">
             <img src="<?php echo Yii::app()->getBaseUrl(true).'/images/products/323/35_1345813558lbr-UNIA-plug-IBIS-55.jpg'?>" alt="Изображение товара">
         </div>
     </div>
     <div class="one_banner">
-        <h3 val="120-150" label="для 120 л.с.">ДЛЯ 120 ЛОШАДИНЫХ СИЛ</h3>
+        <h3 val="120-150" label="для 120 л.с.">для тракторов 120 л.с.</h3>
         <div class="m_image">
             <img src="<?php echo Yii::app()->getBaseUrl(true).'/images/products/338/64_1324902156LBR_Kos00.jpg'?>" alt="Изображение товара">
         </div>
     </div>
     <div class="one_banner">
-        <h3 val="150-180" label="для 150 л.с.">ДЛЯ 150 ЛОШАДИНЫХ СИЛ</h3>
+        <h3 val="150-180" label="для 150 л.с.">для тракторов 150 л.с.</h3>
         <div class="m_image">
             <img src="<?php echo Yii::app()->getBaseUrl(true).'/images/products/341/kret-plow-00.jpg'?>" alt="Изображение товара">
         </div>
     </div>
     <div class="one_banner">
-        <h3 val="180-230" label="180-230 л.с.">180 - 230 ЛОШАДИНЫХ СИЛ</h3>
+        <h3 val="180-230" label="180-230 л.с.">для тракторов 180 - 230 л.с.</h3>
         <div class="m_image">
             <img src="<?php echo Yii::app()->getBaseUrl(true).'/images/products/598/lbr-pallada-00.jpg'//$images['229']?>" alt="Изображение товара">
         </div>
     </div>
     <div class="one_banner">
-        <h3 val="250-350" label="250-350 л.с.">250 - 350 ЛОШАДИНЫХ СИЛ</h3>
+        <h3 val="250-350" label="250-350 л.с.">для тракторов 250 - 350 л.с.</h3>
         <div class="m_image">
             <img src="<?php echo Yii::app()->getBaseUrl(true).'/images/products/322/42_1324369833Hektor00.jpg'//$images['349']?>" alt="Изображение товара">
         </div>
     </div>
     <div class="one_banner">
-        <h3 val="350-1000" label="свыше 350 л.с.">СВЫШЕ 350 ЛОШАДИНЫХ СИЛ</h3>
+        <h3 val="350-1000" label="свыше 350 л.с.">для тракторов свыше 350 л.с.</h3>
         <div class="m_image">
             <img src="<?php echo Yii::app()->getBaseUrl(true).'/images/products/307/50_1324371222LBR_superharrows_Summers00.jpg'//$images['1000']?>" alt="Изображение товара">
         </div>
