8586a2c20edab0002650480e412e3c389f5e1ad7
Tatsyana Krylova <krilova@lbr.ru>
2014-08-08 14:40:06 +0300

    url for mightiness

--- protected/controllers/MightinessController.php
+++ protected/controllers/MightinessController.php
@@ -5,37 +5,37 @@ class MightinessController extends Controller
     {
         if(empty($sort)){
             $response = '<div class="one_banner">
-                    <h3 val="80-120" label="80 л.с.">для тракторов 80 л.с.</h3>
+                    <h3><a href="/selskohozyaystvennaya-tehnika/mightiness/sort/mashiny-dlya-traktora-80-l.s./">для тракторов 80 л.с.</a></h3>
                     <div class="m_image">
                         <img src="'.Yii::app()->getBaseUrl(true).'/images/products/323/35_1345813558lbr-UNIA-plug-IBIS-55.jpg" alt="Изображение товара">
                     </div>
                 </div>
                 <div class="one_banner">
-                    <h3 val="120-150" label="120 л.с.">для тракторов 120 л.с.</h3>
+                    <h3><a href="/selskohozyaystvennaya-tehnika/mightiness/sort/mashiny-dlya-traktora-120-l.s./">для тракторов 120 л.с.</a></h3>
                     <div class="m_image">
                         <img src="'.Yii::app()->getBaseUrl(true).'/images/products/338/64_1324902156LBR_Kos00.jpg" alt="Изображение товара">
                     </div>
                 </div>
                 <div class="one_banner">
-                    <h3 val="150-180" label="150 л.с.">для тракторов 150 л.с.</h3>
+                    <h3><a href="/selskohozyaystvennaya-tehnika/mightiness/sort/mashiny-dlya-traktora-150-l.s./">для тракторов 150 л.с.</a></h3>
                     <div class="m_image">
                         <img src="'.Yii::app()->getBaseUrl(true).'/images/products/341/kret-plow-00.jpg" alt="Изображение товара">
                     </div>
                 </div>
                 <div class="one_banner">
-                    <h3 val="180-230" label="180-230 л.с.">для тракторов 180 - 230 л.с.</h3>
+                    <h3><a href="/selskohozyaystvennaya-tehnika/mightiness/sort/mashiny-dlya-traktora-180-230-l.s./">для тракторов 180 - 230 л.с.</a></h3>
                     <div class="m_image">
                         <img src="'.Yii::app()->getBaseUrl(true).'/images/products/324/36_1324367139Unia Vis XL 00.jpg" alt="Изображение товара">
                     </div>
                 </div>
                 <div class="one_banner">
-                    <h3 val="250-350" label="250-350 л.с.">для тракторов 250 - 350 л.с.</h3>
+                    <h3><a href="/selskohozyaystvennaya-tehnika/mightiness/sort/mashiny-dlya-traktora-250-350-l.s./">для тракторов 250 - 350 л.с.</a></h3>
                     <div class="m_image">
                         <img src="'.Yii::app()->getBaseUrl(true).'/images/products/322/42_1324369833Hektor00.jpg" alt="Изображение товара">
                     </div>
                 </div>
                 <div class="one_banner">
-                    <h3 val="350-1000" label="свыше 350 л.с.">для тракторов свыше 350 л.с.</h3>
+                    <h3><a href="/selskohozyaystvennaya-tehnika/mightiness/sort/mashiny-dlya-traktora-svyshe-350-l.s./">для тракторов свыше 350 л.с.</a></h3>
                     <div class="m_image">
                         <img src="'.Yii::app()->getBaseUrl(true).'/images/products/307/50_1324371222LBR_superharrows_Summers00.jpg" alt="Изображение товара">
                     </div>
