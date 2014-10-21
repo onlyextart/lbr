f04b2e84a9164deb5a2de00d7155ecdd1f882775
YURY <vasiliyan@lbr.ru>
2014-10-21 11:33:50 +0300

    hidden link

--- js/site/hidden_link.js
+++ js/site/hidden_link.js
@@ -1,11 +1,7 @@
-$(function(){
-    $('#footer1').load('http://www.lbr.ru/tehnika/teleskopicheskie-pogruzchiki/rabochie-organy-dieci/595-rabochie-organy-k-teleskopicheskim-pogruzchikam-dieci/', function() {
-        // ...
-    });
-});
-
-$(function(){
-    $('#footer2').load('http://site.ru/uploads/files/footer2.html', function() {
-        // ...
+jQuery(document).ready(function($) {
+    $('.mylink').replaceWith(function(){
+        return '<a href="' + $(this).attr('data-url')
+            + '" title="' + $(this).attr('title')
+            + '">' + $(this).html() + '</a>';
     });
 });
