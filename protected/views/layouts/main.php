01e00e588cb5bb66db41d2077d7c8fd807c7ee00
Tatsyana Krylova <krilova@lbr.ru>
2014-10-30 09:27:57 +0300

    favicon

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -37,6 +37,7 @@
         <meta name="description" content="<?php echo $meta_description; ?>">
         <title><?php echo $meta_title; ?></title>
         <meta name="google-site-verification" content="PZIP2B6qRbs7ZY_9ta4msuDLA57hDzruB3irakMCHAQ" />
+        <link rel="shortcut icon" type="image/jpg" href="<?php echo Yii::app()->request->baseUrl.'/images/favicon.jpg';?>"/>
         <?php
         // Добавление css и javascript на страницу сайта
 //        Yii::app()->clientScript->registerCssFile('/css/ui/custom-theme/jquery-ui-1.9.2.custom.min.css');
