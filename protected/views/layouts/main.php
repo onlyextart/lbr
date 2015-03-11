2f0dda4b63ba9ffa23afbb9a11e84d06fbf1d632
YURY <vasiliyan@lbr.ru>
2015-03-11 14:43:59 +0300

    icon

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,7 +1,7 @@
 <?php
 /* @var $this Controller */
-//$less = new lessc;
-//$less->compileFile($_SERVER['DOCUMENT_ROOT'].'/css/input.less', $_SERVER['DOCUMENT_ROOT'].'/css/main.css');
+$less = new lessc;
+$less->compileFile($_SERVER['DOCUMENT_ROOT'].'/css/input.less', $_SERVER['DOCUMENT_ROOT'].'/css/main.css');
 ?>
 
 <!DOCTYPE html>
