ec46f9f500ea7218a2351235c07424b1b5f847ae
YURY <vasiliyan@lbr.ru>
2015-03-12 16:58:17 +0300

    favicon

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -37,7 +37,8 @@ $less->compileFile($_SERVER['DOCUMENT_ROOT'].'/css/input.less', $_SERVER['DOCUME
         <meta name="description" content="<?php echo $meta_description; ?>">
         <title><?php echo $meta_title; ?></title>
         <meta name="google-site-verification" content="PZIP2B6qRbs7ZY_9ta4msuDLA57hDzruB3irakMCHAQ" />
-        <?php
+        <link rel="shortcut icon" type="image/jpg" href="<?php echo Yii::app()->request->baseUrl.'/images/favicon.jpg';?>"/>
+            <?php
         // Добавление css и javascript на страницу сайта
 //        Yii::app()->clientScript->registerCssFile('/css/ui/custom-theme/jquery-ui-1.9.2.custom.min.css');
         Yii::app()->clientScript->registerCssFile('/css/main.css?'.time());
