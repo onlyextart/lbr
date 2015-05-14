50d5445e1565a1793bd9a8acd7fc6d2369dba34d
Vasiliyan Yury <vasiliyan@lbr.ru>
2015-05-14 15:53:45 +0300

    link with product on sale

--- protected/views/products/review_image.php
+++ protected/views/products/review_image.php
@@ -5,7 +5,7 @@ if ($data->image_type=='1' || $data->image_type=='2' || $data->image_type=='3')
     $class_ = $data->image_type!='2'? '' : 'action';
     echo '<div class="review_photo time_to_buy '.$class_.'">';
     echo '<img src="'.$data->image.'" alt="'.$data->name.'" title="'.$data->name.'">';
-    echo $data->image_top_left ? '<span class="time_to_buy_top_left">'.$data->image_top_left.'</span>':'';
+    echo $data->image_top_left ? '<a href="http://www.lbr.ru/akcii/"><span class="time_to_buy_top_left">'.$data->image_top_left.'</span></a>':'';
     echo $data->image_top_right ? '<img src="/images/timetobuy/subsid.png" alt="Субсидируется" class="time_to_buy_top_right">':'';
     
     if($data->image_type=='3')
