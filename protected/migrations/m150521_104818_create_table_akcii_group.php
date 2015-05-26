42e13722abaa9df12038c42d0ff52695f1845d46
teterukova <teterukova@lbr.ru>
2015-05-26 13:57:41 +0300

    edit migration create table akcii_group

--- protected/migrations/m150521_104818_create_table_akcii_group.php
+++ protected/migrations/m150521_104818_create_table_akcii_group.php
@@ -7,18 +7,13 @@ class m150521_104818_create_table_akcii_group extends CDbMigration
             $transaction=$this->getDbConnection()->beginTransaction();
             try
             {
-               $sql ="
-                     create table IF NOT EXISTS 'akcii_group'
-                     (
-                       'id'                     int not null,
-                       'item_id'                int not null,
-                       'description'            text,
-                       'published'              int,
-                       'range'                  int,
-                        primary key ('id'),
-                        foreign key ('item_id') references 'menu_items' ('id') on delete cascade on update cascade
-                      );";
-               $this->execute($sql);
+                $this->createTable('akcii_group',array(
+                    'id' => 'pk',
+                    'item_id' => 'INTEGER CONSTRAINT [item_id] REFERENCES [menu_items]([id]) ON DELETE CASCADE ON UPDATE CASCADE',
+                    'description' => 'text',
+                    'published' => 'int',
+                    'range' => 'int',
+                ));
                $transaction->commit();
             }
             catch(Exception $e)
