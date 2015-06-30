567ff39c49c84cd0d82bdfb20382720f05ff0bff
Vasiliyan Yury <vasiliyan@lbr.ru>
2015-06-30 10:50:14 +0300

    add operator onlinesaler

--- protected/extensions/dieciChatWidget/views/index.php
+++ protected/extensions/dieciChatWidget/views/index.php
@@ -8,6 +8,6 @@ else{
 	$filialName .= '.';	
 }
 ?>
-<script>mo_manager="a1d50185e7426cbb0acad1e6ca74b9aa";</script>
+<script>mo_manager="907edb0aa6986220dbffb79a788596ee";</script>
 <script type="text/javascript">var _oaq = _oaq || [];_oaq.push(['_OPAccount', '716']);(function() {var oa = document.createElement('script'); oa.type = 'text/javascript'; oa.async = true; oa.src = 'http://onlinesaler.ru/js/common/js.php?domen=<?php echo $filialName ?>lbr.ru&id=716'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(oa, s); })();</script>
 <!--<?php echo $filialName ?>-->
