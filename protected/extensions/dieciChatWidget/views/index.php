bbb8de6c3ebacd61ce6804fd34adaa04b0a8dd7a
Vasiliyan Yury <vasiliyan@lbr.ru>
2015-07-02 10:32:19 +0300

    edit session

--- protected/extensions/dieciChatWidget/views/index.php
+++ protected/extensions/dieciChatWidget/views/index.php
@@ -8,6 +8,7 @@ else{
 	$filialName .= '.';	
 }
 ?>
+<script>om_unickey="PHPSESSID";</script>
 <script>mo_manager="907edb0aa6986220dbffb79a788596ee";</script>
 <script type="text/javascript">var _oaq = _oaq || [];_oaq.push(['_OPAccount', '716']);(function() {var oa = document.createElement('script'); oa.type = 'text/javascript'; oa.async = true; oa.src = 'http://onlinesaler.ru/js/common/js.php?domen=<?php echo $filialName ?>lbr.ru&id=716'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(oa, s); })();</script>
 <!--<?php echo $filialName ?>-->
