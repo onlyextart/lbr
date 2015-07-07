6ffab497f4b0da870a93de916ea3ef243b8e2657
Vasiliyan Yury <vasiliyan@lbr.ru>
2015-07-07 15:26:26 +0300

    edit evp14 v 244

--- protected/views/getkp/tmpl/single/v244.php
+++ protected/views/getkp/tmpl/single/v244.php
@@ -174,22 +174,22 @@
                         <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
                             <tr>
                                 <td width="240" valign="top" style="padding-top: 3px; padding-right: 20px;">
-                                    <a href="http://www.lbr.ru/tehnika/ovoshchi-i-kartofel/skladskaya-obrabotka-sortirovka-khranenie/bunker-pps-20-60/?utm_source=EVP13&utm_medium=email&utm_term=Bunker_priemno-sortirovochnyj_PPS_20-60&utm_content=<?php echo $email;?>&utm_campaign=Bunker_priemno-sortirovochnyj_PPS_20-60" target="_blank" style="font-size: 20px; font-weight: 800; font-family: 'Arial Black', Arial; text-decoration: none;">
-                                    <p style="margin:0; padding: 0; font-size: 16px; font-weight: bold;text-align: center;font-family: 'Trebuchet MS', sans-serif; ">
+                                    <a href="http://www.lbr.ru/tehnika/klassicheskie-tekhnologii-pochvoobrabotki-i-seva/zernovye-seyalki/332-zernovye-seyalki-polonez/?utm_source=EVP14&utm_medium=email&utm_term=zernovye_sejalki_polonez&utm_content=<?php echo $email;?>&utm_campaign=zernovye_sejalki_polonez" target="_blank">
+                                        <p style="margin:0; padding: 0; font-size: 16px; font-weight: bold;text-align: center;font-family: 'Trebuchet MS', sans-serif; ">
                                         СЕЯЛКИ POLONEZ/POLONEZ DUBLO
                                     </p>
                                     </a>
                                 </td>
                                 <td width="240" valign="top" style="padding-top: 3px; padding-right: 20px; text-align: center;">
-                                     <a href="http://www.lbr.ru/tehnika/ovoshchi-i-kartofel/skladskaya-obrabotka-sortirovka-khranenie/konveyer-teleskopicheskiy-kt-40/?utm_source=EVP13&utm_medium=email&utm_term=Konvejer_teleskopicheskij_KT-40&utm_content=<?php echo $email;?>&utm_campaign=Konvejer_teleskopicheskij_KT" target="_blank" style="font-size: 20px; font-weight: 800; font-family: 'Arial Black', Arial; text-decoration: none;">
+                                     <a href="http://www.lbr.ru/tehnika/klassicheskie-tekhnologii-pochvoobrabotki-i-seva/zernovye-seyalki/333-zernovye-seyalki-mazur/?utm_source=EVP14&utm_medium=email&utm_term=zernovye_sejalki_mazur&utm_content=<?php echo $email;?>&utm_campaign=zernovye_sejalki_mazur" target="_blank">
                                      <p style="margin:0; padding: 0; font-size: 16px; font-weight: bold; text-align: center; font-family: 'Trebuchet MS', sans-serif;">
                                         СЕЯЛКИ MAZUR/MAZUR DUBLO
                                     </p>
                                     </a>
                                 </td>
                                 <td width="240" valign="top" style="padding-top: 3px; padding-left: 10px; text-align: center;">
-                                     <a href="http://www.lbr.ru/tehnika/ovoshchi-i-kartofel/skladskaya-obrabotka-sortirovka-khranenie/zagruzchik-teleskopicheskiy-zt-40/?utm_source=EVP13&utm_medium=email&utm_term=Zagruzchik_dlja_ovoshhej_teleskopicheskij_ZT-40&utm_content=<?php echo $email;?>&utm_campaign=Zagruzchik_dlja_ovoshhej_teleskopicheskij_ZT-40" target="_blank" style="font-size: 20px; font-weight: 800; font-family: 'Arial Black', Arial; text-decoration: none;">
-                                     <p style="margin:0; padding: 0; font-size: 16px; font-weight: bold; text-align: center; font-family: 'Trebuchet MS', sans-serif;">
+                                     <a href="http://www.lbr.ru/tehnika/klassicheskie-tekhnologii-pochvoobrabotki-i-seva/prikatyvayushchie-katki/657-prikatyvayushchie-katki-expom/?utm_source=EVP14&utm_medium=email&utm_term=prikatyvajushhie_katki_expom&utm_content=<?php echo $email;?>&utm_campaign=prikatyvajushhie_katki_expom" target="_blank">
+                                    <p style="margin:0; padding: 0; font-size: 16px; font-weight: bold; text-align: center; font-family: 'Trebuchet MS', sans-serif;">
                                         ПРИКАТЫВАЮЩИЕ КАТКИ JACEK И MAXIMUS
                                     </p>
                                     </a>
@@ -203,16 +203,19 @@
                         <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
                             <tr>
                                 <td width="240" valign="top" style="padding-top: 3px; padding-right: 20px;">
-                                    <img src="http://www.lbr.ru/images/kp/244-evp14/244-evp-14_12.jpg" width="240" style="border: 0; text-align: right;" alt="Изображения не отображаются">
-                                    
+                                    <a href="http://www.lbr.ru/tehnika/klassicheskie-tekhnologii-pochvoobrabotki-i-seva/zernovye-seyalki/332-zernovye-seyalki-polonez/?utm_source=EVP14&utm_medium=email&utm_term=zernovye_sejalki_polonez&utm_content=<?php echo $email;?>&utm_campaign=zernovye_sejalki_polonez" target="_blank">
+                                        <img src="http://www.lbr.ru/images/kp/244-evp14/244-evp-14_12.jpg" width="240" style="border: 0; text-align: right;" alt="Изображения не отображаются">
+                                    </a>
                                 </td>
                                 <td width="240" valign="top" style="padding-top: 3px; padding-right: 20px; text-align: left;">
+                                     <a href="http://www.lbr.ru/tehnika/klassicheskie-tekhnologii-pochvoobrabotki-i-seva/zernovye-seyalki/333-zernovye-seyalki-mazur/?utm_source=EVP14&utm_medium=email&utm_term=zernovye_sejalki_mazur&utm_content=<?php echo $email;?>&utm_campaign=zernovye_sejalki_mazur" target="_blank">
                                      <img src="http://www.lbr.ru/images/kp/244-evp14/244-evp-14_1.jpg" width="240" style="border: 0; text-align: right;" alt="Изображения не отображаются">
-                                    
+                                    </a>
                                 </td>
                                 <td width="240" valign="top" style="padding-top: 3px; text-align: left;">
-                                     <img src="http://www.lbr.ru/images/kp/244-evp14/244-evp-14_2.jpg" width="240" style="border: 0; text-align: right;" alt="Изображения не отображаются">
-                                    
+                                    <a href="http://www.lbr.ru/tehnika/klassicheskie-tekhnologii-pochvoobrabotki-i-seva/prikatyvayushchie-katki/657-prikatyvayushchie-katki-expom/?utm_source=EVP14&utm_medium=email&utm_term=prikatyvajushhie_katki_expom&utm_content=<?php echo $email;?>&utm_campaign=prikatyvajushhie_katki_expom" target="_blank">
+                                    <img src="http://www.lbr.ru/images/kp/244-evp14/244-evp-14_2.jpg" width="240" style="border: 0; text-align: right;" alt="Изображения не отображаются">
+                                    </a>
                                 </td>
                             </tr>
                         </table>
@@ -291,32 +294,90 @@
                         </table>
                     </td>
                 </tr>
-                
+                <tr>
+                    <td style="padding: 10px 20px 0px 20px;">
+                        <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                            <tr>
+                                <td style="padding-top: 3px;">
+                                    <hr />
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
                 <tr>
                     <td style="padding:20px 20px 10px 20px;">
                         <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
                             <tr>
                                 <td width="190" align="center" style="padding-top: 3px;">
                                     <a href="http://www.lbr.ru/?utm_source=EVP14&utm_medium=email&utm_term=glavnaya&utm_content=<?php echo $email;?>&utm_campaign=glavnaya" title="" style=" text-decoration: none; border: 0;">
-                                        <img src="http://www.lbr.ru/images/kp/244-evp14/tehnika.png" align="center" width="90" style="border: 0;" alt="Изображения не отображаются">
+                                        <img src="http://www.lbr.ru/images/kp/244-evp14/tehnika.png" align="center" width="45" style="border: 0;" alt="Изображения не отображаются">
                                     </a>
                                 </td>
                                 <td width="190" align="center" style="padding-top: 3px;">
                                     <a href="http://lbr-market.ru/?utm_source=EVP14&utm_medium=email&utm_term=glavnaya_zapchasti&utm_content=<?php echo $email;?>&utm_campaign=glavnaya_zapchasti" title="" style=" text-decoration: none; border: 0;">
-                                        <img src="http://www.lbr.ru/images/kp/244-evp14/spareparts.png" width="90" align="center" style="border: 0;" alt="Изображения не отображаются">
+                                        <img src="http://www.lbr.ru/images/kp/244-evp14/spareparts.png" width="45" align="center" style="border: 0;" alt="Изображения не отображаются">
                                     </a>
                                 </td>
                                 <td width="190" align="center" style="padding-top: 3px;">
                                     <a href="http://www.lbr.ru/akcii/?utm_source=EVP14&utm_medium=email&utm_term=banner_2_podval&utm_content=<?php echo $email;?>&utm_campaign=banner_2_podval" title="" style=" text-decoration: none; border: 0;">
-                                        <img src="http://www.lbr.ru/images/kp/244-evp14/akcii.png" width="90" align="center" style="border: 0;" alt="Изображения не отображаются">
+                                        <img src="http://www.lbr.ru/images/kp/244-evp14/akcii.png" width="45" align="center" style="border: 0;" alt="Изображения не отображаются">
                                     </a>
                                 </td>
                                 <td width="190" align="center" style="padding-top: 3px;">
                                     <a href="https://www.youtube.com/channel/UC5nzN2jjNZU2uWB3-r9siCg?utm_source=EVP14&utm_medium=email&utm_term=youtube_lbr&utm_content=<?php echo $email;?>&utm_campaign=youtube_lbr" title="" style=" text-decoration: none; border: 0;">
-                                        <img src="http://www.lbr.ru/images/kp/244-evp14/youtube.png" width="90" align="center" style="border: 0;" alt="Изображения не отображаются">
+                                        <img src="http://www.lbr.ru/images/kp/244-evp14/youtube.png" width="45" align="center" style="border: 0;" alt="Изображения не отображаются">
                                     </a>
                                 </td>
                                 
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding:0px 20px 10px 20px;">
+                        <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                            <tr>
+                                <td width="190" align="center" style="padding-top: 3px;">
+                                    <a href="http://www.lbr.ru/?utm_source=EVP14&utm_medium=email&utm_term=glavnaya&utm_content=<?php echo $email;?>&utm_campaign=glavnaya" title="" style=" text-decoration: none; border: 0;">
+                                        <p style="margin:0; padding: 0; font-size: 14px; text-align: center; font-family: 'Trebuchet MS', sans-serif;">
+                                        Сельхозтехника
+                                    </p>
+                                    </a>
+                                </td>
+                                <td width="190" align="center" style="padding-top: 3px;">
+                                    <a href="http://lbr-market.ru/?utm_source=EVP14&utm_medium=email&utm_term=glavnaya_zapchasti&utm_content=<?php echo $email;?>&utm_campaign=glavnaya_zapchasti" title="" style=" text-decoration: none; border: 0;">
+                                        <p style="margin:0; padding: 0; font-size: 14px; text-align: center; font-family: 'Trebuchet MS', sans-serif;">
+                                        Запчасти
+                                    </p>
+                                    </a>
+                                </td>
+                                <td width="190" align="center" style="padding-top: 3px;">
+                                    <a href="http://www.lbr.ru/akcii/?utm_source=EVP14&utm_medium=email&utm_term=banner_2_podval&utm_content=<?php echo $email;?>&utm_campaign=banner_2_podval" title="" style=" text-decoration: none; border: 0;">
+                                        <p style="margin:0; padding: 0; font-size: 14px; text-align: center; font-family: 'Trebuchet MS', sans-serif;">
+                                        Акции и скидки
+                                    </p>
+                                    </a>
+                                </td>
+                                <td width="190" align="center" style="padding-top: 3px;">
+                                    <a href="https://www.youtube.com/channel/UC5nzN2jjNZU2uWB3-r9siCg?utm_source=EVP14&utm_medium=email&utm_term=youtube_lbr&utm_content=<?php echo $email;?>&utm_campaign=youtube_lbr" title="" style=" text-decoration: none; border: 0;">
+                                        <p style="margin:0; padding: 0; font-size: 14px; text-align: center; font-family: 'Trebuchet MS', sans-serif;">
+                                        YouTube
+                                    </p>
+                                    </a>
+                                </td>
+                                
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 10px 20px 0px 20px;">
+                        <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                            <tr>
+                                <td style="padding-top: 3px;">
+                                    <hr />
+                                </td>
                             </tr>
                         </table>
                     </td>
@@ -337,7 +398,7 @@
 <table width="800" bgcolor="#eeeeee" cellspacing="0" cellpadding="0" align="center" style="border: 0; border-collapse: collapse;">
                 
                 <tr>
-                    <td style="padding: 0 20px; background: white;">
+                    <td style="padding: 20px; background: white;">
                        <table style="border-collapse: collapse;" width="760" cellspacing="0" cellpadding="0" align="left" bgcolor="#FFFFFF">
                             <tr>
                                 <td><span style="font-family: 'Trebuchet MS', sans-serif; font-size: 15px; font-weight: bold;">Ждем Вас в филиале ЛБР-АгроМаркет (<b><?php echo $filial['name']; ?></b>)!</span><br></td>
