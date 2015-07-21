3cad9f4a4b3e996933f4b69085eadcde09187bcd
teterukova <teterukova@lbr.ru>
2015-07-21 13:59:07 +0300

    Edit footer

--- protected/views/getkp/tmpl/single/v246.php
+++ protected/views/getkp/tmpl/single/v246.php
@@ -327,13 +327,7 @@
 </table>
 <table width="800" bgcolor="#eeeeee" cellspacing="0" cellpadding="0" align="center" style="border: 0; border-collapse: collapse;">
                 <tr>
-                    <td style="padding: 25px 20px 10px 20px; background: white;">
-                        <p style="margin: 0; padding: 0; font-family: 'Trebuchet MS', sans-serif; font-size: 13px;">Всю подробную информацию по заинтересовавшим Вас моделям техники, а также опыт эксплуатации и отзывы потребителей Вы сможете получить, обратившись в наш филиал. </p>
-                    </td>
-                </tr>
-               
-                <tr>
-                    <td style="padding: 0 20px; background: white;">
+                    <td style="padding: 20px 20px 0px 20px; background: white;">
                        <table style="border-collapse: collapse;" width="760" cellspacing="0" cellpadding="0" align="left" bgcolor="#FFFFFF">
                             <tr>
                                 <td><span style="font-family: 'Trebuchet MS', sans-serif; font-size: 15px; font-weight: bold;">Ждем Вас в филиале ЛБР-АгроМаркет (<b><?php echo $filial['name']; ?></b>)!</span><br></td>
