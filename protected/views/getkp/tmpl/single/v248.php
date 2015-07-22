01d10c7fa1d1e2c64c3dad2fa3bf96fcb22c03de
teterukova <teterukova@lbr.ru>
2015-07-22 15:40:43 +0300

    test

--- protected/views/getkp/tmpl/single/v248.php
+++ protected/views/getkp/tmpl/single/v248.php
@@ -1,7 +1,9 @@
 <?php
     $email=str_replace(";","_",$data['email']);
     $email=str_replace(" ","",$email);
-
+    $email1="";
+    $email2="test1_mail@lbr.ru";
+    $email3="test1_mail@lbr.ru_test2_mail@lbr.ru";
 ?>
 
 <head>
@@ -17,8 +19,10 @@
         p{mso-line-height-rule: exactly !important; line-height: 13pt;}
         a, a:active, a:visited{color:black;}
     </style>
-    <a href="http://www.lbr.ru/akcii/?sb=test_cookie&utm_medium=email&utm_term=test&ct=<?php echo SecurityController::encrypt($email);?>&lk=test" target="_blank">Ссылка</a><br>
-      
+    <a href="http://www.lbr.ru/akcii/?sb=test_cookie&utm_medium=email&utm_term=test&ct=<?php echo SecurityController::encrypt($email1);?>&lk=test" target="_blank">Ссылка1</a><br>
+    <a href="http://www.lbr.ru/akcii/?sb=test_cookie&utm_medium=email&utm_term=test&ct=<?php echo SecurityController::encrypt($email2);?>&lk=test" target="_blank">Ссылка2</a><br>
+    <a href="http://www.lbr.ru/akcii/?sb=test_cookie&utm_medium=email&utm_term=test&ct=<?php echo SecurityController::encrypt($email3);?>&lk=test" target="_blank">Ссылка c 2 email</a><br>
+       
 <!-- Background grey; -->
 </body>
 
