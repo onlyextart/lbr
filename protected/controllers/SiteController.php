ce8510f5ea464f375fd220060f141cd639f593af
Tatsyana Krylova <krilova@lbr.ru>
2015-07-24 15:30:24 +0300

    test

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -107,13 +107,13 @@ class SiteController extends Controller
     /**
      * Analitics
      */
-    
+    /*
     public function actionSaveAnalitics() 
     {
         $url = Yii::app()->request->getPost('url');
         $cookies = Yii::app()->request->cookies;
         //if(Yii::app()->user->isGuest && !strpos($url, '/users/login')) {
-        if(!Yii::app()->user->isGuest) {
+        if(Yii::app()->user->isGuest) {
             $model = new Analitics;
             $model->time = Yii::app()->request->getPost('time');
             $model->date_created = date('Y-m-d H:i:s');
@@ -129,16 +129,16 @@ class SiteController extends Controller
             if (isset($cookies['ct'])) {
                 $model->customer_id = SecurityController::decrypt($cookies['ct']->value);
             } else 
-                $model->customer_id = "can't get customer id";
+                $model->customer_id = "can't get customer id - ".Yii::app()->request->getPost('ct');
 
             if (isset($cookies['sb'])) {
                 $model->subscription_id = $cookies['sb']->value;
-            } else $model->subscription_id = "can't get subscription id";
+            } else $model->subscription_id = "can't get subscription id - ".Yii::app()->request->getPost('sb');
             
             $model->save();
         }
     }
-    
+    */
 
     public function actionDelAnalitics() 
     {
@@ -179,7 +179,7 @@ class SiteController extends Controller
         $model->save();
     }
     
-    /*public function actionSaveAnalitics() 
+    public function actionSaveAnalitics() 
     {
         if(!Yii::app()->user->isGuest) {
             $model = new Analitics;
@@ -204,5 +204,5 @@ class SiteController extends Controller
             $model->save();
             Yii::log('6', 'info');
         }
-    }*/
+    }
 }
