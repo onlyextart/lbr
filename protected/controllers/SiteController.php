39ffda7a7bc29b139a9f04a4f2088f8c2b3209dc
Tatsyana Krylova <krilova@lbr.ru>
2015-07-24 15:20:45 +0300

    test

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -185,7 +185,7 @@ class SiteController extends Controller
             $model = new Analitics;
             Yii::log('1', 'info');
             //================================
-            /*$cookies = Yii::app()->request->cookies;
+            $cookies = Yii::app()->request->cookies;
             Yii::log('2', 'info');
             if ($cookies['ct']) {
                 //$test = SecurityController::encrypt('test1_mail@lbr.ru_test2_mail@lbr.ru');
@@ -194,7 +194,7 @@ class SiteController extends Controller
                 Yii::log('3', 'info');
                 $model->customer_id = SecurityController::decrypt($cookies['ct']->value);
             } else $model->customer_id = "can't get customer id";
-            */
+            
             Yii::log('4', 'info');
             $model->time = Yii::app()->request->getPost('time');
             $model->url = Yii::app()->request->getPost('url');
