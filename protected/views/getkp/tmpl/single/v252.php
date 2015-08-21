82a166b34dcd01e5b067eeaf3bda0209b764451d
teterukova <teterukova@lbr.ru>
2015-08-21 14:24:07 +0300

    EVP_market2

---
+++ protected/views/getkp/tmpl/single/v252.php
@@ -0,0 +1,250 @@
+<?php
+$email = str_replace(";", "_", $data['email']);
+$email = str_replace(" ", "", $email);
+?>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <style>
+        p{mso-line-height-rule:exactly !important; line-height: 13pt;}
+        a, a:active, a:visited{color:black;}
+    </style>
+</head>
+<body style="background: #eeeeee; margin: 0; padding: 0; border: 0;">
+<style>
+    p{mso-line-height-rule: exactly !important; line-height: 13pt;}
+    a, a:active, a:visited{color:black;}
+</style>
+
+<!-- Background grey -->
+<table style="background: #eeeeee; margin: 0; padding: 0; border: 0; width: 100%;">
+    <tr>
+        <td style="padding:0">
+            <table width="800" bgcolor="#ffffff" cellspacing="0" cellpadding="0" align="center" style="border: 0; border-collapse: collapse;">
+                <tr>
+                    <td style="padding: 0px 0px 0px 0px;">
+                        <table width="800" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                            <tr>
+                                <td style="padding: 10px 20px 0px 20px;">
+                                    <a href="http://lbr-market.ru/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=glavnaya" target="_blank"><img src="http://www.lbr.ru/images/kp/252-evpZ2/1.jpg" width="760" style="border: 0; float: left;" alt="Изображения не отображаются"></a>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table width="800" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                            <tr>
+                                <td style="padding: 10px 20px 0px 20px;">
+                                    <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: #FFFFFF;">
+                                        <tr>
+                                            <td style="padding: 10px 0px 0px 20px; width: 100px;" valign="bottom">
+                                                <img src="http://www.lbr.ru/images/kp/252-evpZ2/logo.jpg" width="80"  style="border: 0; float: left;" alt="Логотип ЛБР-Агромаркет">
+                                            </td>
+                                            <td valign="top" style="width: 660px;  padding:10px 20px 0px 10px;text-align: center;">
+                                                <span style="margin: 0; padding: 0; color: #6e7071; background: none !important; font-size: 14px; font-weight: normal; font-family: 'Myriad Pro', Arial Narrow, Arial, Helvetica,sans-serif; outline: none">
+                                                    <?php echo $data['header']; ?>
+                                                </span>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td style="padding:0;">
+                        <!-- Первый блок -->
+                        <table width="800" bgcolor="#FFFFFF" cellspacing="0" cellpadding="0" align="center" style="border: 0; border-collapse: collapse;">
+                            <tr>
+                                <td>
+                                    <table width="800" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                                        <tr>
+                                            <td width="267" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/4785-segment-nozha-41822-02/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Segment_nozha_41822_02" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/2.jpg" width="267" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                            <td width="266" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/5857-golovka-nozha-626411-1r/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Golovka_nozha_626411_1" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/3.jpg" width="266" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                            <td width="267" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/45789-fil-tr-gidravlicheskiy-hf6553/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Filtr_gidravlicheskij_HF6553" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/4.jpg" width="267" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td width="267" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/5844-palec-protivorezhuschiy-80379720r/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Palec_protivorezhushhij_80379720R" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/5.jpg" width="267" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                            <td width="266" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/5628-palec-protivorezhuschiy-dvoynoy-42849-11/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Palec_protivorezhushhij_dvojnoj_118344" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/6.jpg" width="266" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                            <td width="267" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/11043-remen-privoda-ochistnogo-ventilyatora-301146a1c/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Remen_privoda_ochistnogo_ventiljatora_301146A1C" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/7.jpg" width="267" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                        </tr>
+
+
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td style="padding:10px 0px; border:0;">
+                        <a href="http://lbr-market.ru/site/login/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=banner_reg" target="_blank">
+                            <img src="http://www.lbr.ru/images/kp/252-evpZ2/banner1.jpg" width="800" style="border: 0; float: left;" alt="Изображения не отображаются">
+                        </a>
+                    </td>
+                </tr>
+                
+                
+                <tr>
+                    <td style="padding:0;">
+                        <!-- Второй блок -->
+                        <table width="800" bgcolor="#FFFFFF" cellspacing="0" cellpadding="0" align="center" style="border: 0; border-collapse: collapse;">
+                            <tr>
+                                <td>
+                                    <table width="800" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                                        <tr>
+                                            <td width="267" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/17849-lenta-zhatki-1008948-1ft-0-3048m/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Lenta_zhatki_145960" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/8.jpg" width="267" style="border: 0; float: right;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                            <td width="266" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/5749-maslo-motornoe-akcela-1-engine-oil-15w-40-60l/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Maslo_motornoe_AKCELA_ENGINE_OIL_15W_40" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/9.jpg" width="266" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                            <td width="267" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/5860-klipsa-626749-1r/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Prizhim_nozha_626749_1" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/10.jpg" width="267" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                             </tr>
+                             <tr>
+                                 <td>
+                                     <table width="800" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                                        <tr>
+                                            <td width="267" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/14344-nozh-zhatki-v-sbore-371622a3/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Nozh_zhatki_25_FT_v_sbore_371621A3" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/11.jpg" width="267" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                            <td width="266" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/58316-fil-tr-gidravlicheskiy-hf6557/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Filtr_gidravlicheskij_HF6557" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/12.jpg" width="266" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                            <td width="267" style="padding:10px 0px 0px 0px;">
+                                                <a href="http://lbr-market.ru/sparepart/5867-konveyer-v-sbore-84207192r/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Konvejer_v_sbore_84207192R" target="_blank">
+                                                    <img src="http://www.lbr.ru/images/kp/252-evpZ2/13.jpg" width="267" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                </a>
+                                            </td>
+                                        </tr>
+
+
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td style="padding:15px 0px 0px 0px; border:0;">
+                         <img src="http://www.lbr.ru/images/kp/252-evpZ2/bestoffer.jpg" width="800" style="border: 0; float: left;" alt="Изображения не отображаются">
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td style="padding:10px 20px 0px 20px; border:0;">
+                        <table width="760" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" style="border: 0; border-collapse: collapse; background: #ffffff;">
+                            <tr>
+                                <td style="padding:0px 15px 0px 0px;">
+                                    <a href="http://lbr-market.ru/seasonalsale/index/id/2/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Filtry_FLEETGUARD" target="_blank">
+                                        <img src="http://www.lbr.ru/images/kp/252-evpZ2/14.jpg" width="365" style="border: 0; float: right;" alt="Изображения не отображаются">
+                                    </a>
+                                </td>
+                                <td style="padding:0px 0px 0px 15px;">
+                                    <a href="http://lbr-market.ru/seasonalsale/index/id/6/?sb=EVP_market2&ct=<?php echo SecurityController::encrypt($email);?>&lk=Remni" target="_blank">
+                                        <img src="http://www.lbr.ru/images/kp/252-evpZ2/15.jpg" width="365" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                    </a>
+                                </td>
+                           </tr>
+                        </table>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td style="padding:10px 20px 0px 20px; border:0;">
+                        <hr>
+                    </td>
+                </tr>
+              
+                <tr>
+                    <td>
+                        <table width="800" bgcolor="#eeeeee" cellspacing="0" cellpadding="0" align="center" style="border: 0; border-collapse: collapse;">
+                            <tr>
+                                <td style="padding: 20px 20px 0px 20px; background: white;">
+                                    <table style="border-collapse: collapse;" width="760" cellspacing="0" cellpadding="0" align="left" bgcolor="#FFFFFF">
+                                        <tr>
+                                            <td><span style="font-family: 'Trebuchet MS', sans-serif; font-size: 15px; font-weight: bold;">www.lbr.ru - широкий ассортимент запчастей для сельскохозяйственной техники</span><br></td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding: 5px 20px 0px 20px; background: white;">
+                                    <table style="border-collapse: collapse;" width="760" cellspacing="0" cellpadding="0" align="left" bgcolor="#FFFFFF">
+                                        <tr>
+                                            <td><span style="font-family: 'Trebuchet MS', sans-serif; font-size: 15px;">Телефон для всех регионов России: 8-800-555-32-19</span><br></td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding: 20px 20px; background: white;">
+                                    <table style="border-collapse: collapse;" width="760" cellspacing="0" cellpadding="0" align="center" bgcolor="#FFFFFF">
+                                        <tr>
+                                            <td style="text-align:center;"><span style="font-family: 'Trebuchet MS', sans-serif; font-size: 12px;">2015 (с) ООО "ЛБР-Агромаркет"</span><br></td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <!-- Background grey; -->
+        </td>
+    </tr>
+</table>
+<!-- Background grey; -->
+</body>
+
+
+
+
+
+
