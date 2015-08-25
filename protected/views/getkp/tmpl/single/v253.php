31c5b7c0d24aee412fa636d5c512c7cb184d08f1
teterukova <teterukova@lbr.ru>
2015-08-25 11:38:01 +0300

    EVP17_test

---
+++ protected/views/getkp/tmpl/single/v253.php
@@ -0,0 +1,191 @@
+<?php
+    $email=str_replace(";","_",$data['email']);
+    $email=str_replace(" ","",$email);
+
+?>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <style>
+        p{mso-line-height-rule:exactly !important; line-height: 13pt;}
+        a, a:active, a:visited{color:black;}
+    </style>
+</head>
+<body style="background: #eeeeee; margin: 0; padding: 0; border: 0;">
+    <style>
+        p{mso-line-height-rule: exactly !important; line-height: 13pt;}
+        a, a:active, a:visited{color:black;}
+    </style>
+
+    <!-- Background grey -->
+    <table style="background: #eeeeee; margin: 0; padding: 0; border: 0; width: 100%;">
+        <tr>
+            <td>
+                <table width="800" bgcolor="#eeeeee" cellspacing="0" cellpadding="0" align="center" style="border: 0; border-collapse: collapse;">
+                    <tr>
+                        <td>
+                            <table width="800" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                                <tr>
+                                    <td style="padding: 3px 20px 0px 20px;">
+                                        <a href="" style="cursor:pointer; text-decoration: none; border: 0;" target="_blank">
+                                            <img src="http://www.lbr.ru/images/kp/253-evp17/1.jpg" width="760" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                        </a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                
+                    <tr>
+                        <td>
+                            <table width="800" bgcolor="#eeeeee" cellspacing="0" cellpadding="0" align="center" style="border: 0; border-collapse: collapse;">
+                                <tr>
+                                    <td>
+                                        <table width="800" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                                            <tr>
+                                            <td valign="middle" style="padding: 20px 0px 0px 20px; width: 150px;">
+                                                    <a href="" title="Сайт ЛБР-агромаркет" style=" text-decoration: none; border: 0;">
+                                                        <img src="http://www.lbr.ru/images/kp/default/mail-lbr-logo-orange-small.jpg" width="150"  style="border: 0; float: right;" alt="Логотип ЛБР-Агромаркет">
+                                                    </a>
+                                                </td>
+                                                <td valign="top" style="width: 610px;padding:20px 20px 0px 30px;">
+                                                    <p style="margin: 0; padding: 0; color: #6e7071; background: none !important; font-size: 14px; font-weight: normal; font-family: 'Myriad Pro', Arial Narrow, Arial, Helvetica,sans-serif;outline: none; text-align: center;">
+                                                        <?php echo $data['header']; ?>
+                                                    </p>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <table width="800" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                                           <tr>
+                                                <td style="padding: 10px 20px 0px 20px;">
+                                                    <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                                                        <tr>
+                                                            <td style="padding-top: 3px;">
+                                                                <img src="http://www.lbr.ru/images/kp/253-evp17/descr.jpg" width="760" style="border: 0; float: left;" alt="Изображения не отображаются">
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>    
+                
+                                            <tr>
+                                                <td style="padding:15px 20px 0px 20px;">
+                                                    <p style="margin:0; color: #808080; padding: 0; font-size: 18px; font-weight: normal; text-align: left; line-height:25px; font-family: 'Trebuchet MS', sans-serif;">
+                                                        ЕЩЕ БОЛЬШЕ АКЦИОННОЙ ТЕХНИКИ НА 
+                                                        <a href="" title="Сайт ЛБР-агромаркет" style=" text-decoration: none; border: 0; cursor:pointer;color: #f47a2e;">
+                                                            WWW.LBR.RU
+                                                        </a>
+                                                    </p>
+                                                </td>
+                                            </tr>
+                
+                                            <tr>
+                                                <td style="padding:10px 20px 0px 20px;">
+                                                    <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                                                        <tr>
+                                                            <td width="760" valign="top" style="padding: 0px 0px 20px 0px;text-align: center;">
+                                                                <a href="http://www.lbr.ru/akcii/?utm_source=EVP15&utm_medium=email&utm_term=akcii&utm_content=<?php echo $email;?>&utm_campaign=akcii" style="cursor:pointer; text-decoration: none; border: 0;" target="_blank">
+                                                                    <img src="http://www.lbr.ru/images/kp/245-evp15/banner.jpg" width="760" style="border: 0; float:left;" alt="Изображения не отображаются">
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                
+                                            <tr>
+                                                <td style="padding:0px 20px 20px 20px;">
+                                                    <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: #dfdfdf;">
+                                                        <tr>
+                                                            <td width="195" valign="middle" style="padding: 10px 0px 10px 0px;text-align: center;">
+                                                                <a href="http://www.lbr.ru/selskohozyaystvennaya-tehnika/type/?utm_source=EVP15&utm_medium=email&utm_term=tehnika&utm_content=<?php echo $email;?>&utm_campaign=tehnika" style="cursor:pointer; text-decoration: none; border: 0;" target="_blank">
+                                                                    <p style="margin:0; color: black; padding: 0; font-size: 15px; font-weight: normal; text-align: center; font-family: 'Trebuchet MS', sans-serif;">
+                                                                        <b>Техника</b>
+                                                                    </p>
+                                                                </a>
+                                                            </td>
+                                                            <td width="190" valign="middle" style="padding: 10px 0px 10px 0px;text-align: center;">
+                                                                <a href="http://lbr-market.ru/?utm_source=EVP15&utm_medium=email&utm_term=shop&utm_content=<?php echo $email;?>&utm_campaign=shop" style="cursor:pointer; text-decoration: none; border: 0;" target="_blank">
+                                                                    <p style="margin:0; color: black; padding: 0; font-size: 15px; font-weight: normal; text-align: center; font-family: 'Trebuchet MS', sans-serif;">
+                                                                        <b>Запчасти</b>
+                                                                    </p>
+                                                                </a>
+                                                            </td>
+                                                            <td width="195" valign="middle" style="padding: 10px 0px 10px 0px;text-align: center;">
+                                                                <a href="http://www.lbr.ru/akcii/?utm_source=EVP15&utm_medium=email&utm_term=akcii&utm_content=<?php echo $email;?>&utm_campaign=akcii" style="cursor:pointer; text-decoration: none; border: 0;" target="_blank">
+                                                                    <p style="margin:0; color: black; padding: 0; font-size: 15px; font-weight: normal; text-align: center; font-family: 'Trebuchet MS', sans-serif;">
+                                                                        <b>Акции</b>
+                                                                    </p>
+                                                                </a>
+                                                            </td>
+                                                            <td width="180" valign="middle" style="padding: 10px 0px 10px 0px;text-align: center;">
+                                                                <a href="https://www.youtube.com/channel/UC5nzN2jjNZU2uWB3-r9siCg?utm_source=EVP15&utm_medium=email&utm_term=youtube&utm_content=<?php echo $email;?>&utm_campaign=youtube" style="cursor:pointer; text-decoration: none; border: 0;" target="_blank">
+                                                                    <p style="margin:0; color: black; padding: 0; font-size: 15px; font-weight: normal; text-align: center; font-family: 'Trebuchet MS', sans-serif;">
+                                                                        <b>YouTube</b>
+                                                                    </p>
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td style="padding: 25px 20px 10px 20px; background: white;">
+                            <p style="margin: 0; padding: 0; font-family: 'Trebuchet MS', sans-serif; font-size: 13px;">Всю подробную информацию по заинтересовавшим Вас моделям техники, а также опыт эксплуатации и отзывы потребителей Вы сможете получить у Вашего персонального менеджера. </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 0 20px; background: white;">
+                            <table style="border-collapse: collapse;" width="760" cellspacing="0" cellpadding="0" align="left" bgcolor="#FFFFFF">
+                                <tr>
+                                    <td><span style="font-family: 'Trebuchet MS', sans-serif; font-size: 15px; font-weight: bold;">Ждем Вас в филиале ЛБР-АгроМаркет (<b><?php echo $filial['name']; ?></b>)!</span><br></td>
+                                </tr>
+                                <tr>
+                                    <td><span style="font-family: 'Trebuchet MS', sans-serif; font-size: 13px;"><?php echo $filial['work_time']; ?></span><br></td>
+                                </tr>
+                                <tr>
+                                    <td><span style="font-family: 'Trebuchet MS', sans-serif; font-size: 13px;"><?php echo $filial['address']; ?></span><br></td>
+                                </tr>
+                                <tr>
+                                    <td><span style="font-family: 'Trebuchet MS', sans-serif; font-size: 13px;"><?php echo $filial['phonekp']; ?></span></td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 10px 20px; background: white;">
+                            <?php echo AuthUser::getPasteboard($data['login'], 'email'); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 20px 20px; background: white;">
+                            <p style="margin:0; color: black; padding: 0; font-size: 10px; font-weight: normal; text-align: left; font-family: 'Trebuchet MS', sans-serif;">
+                                Данное письмо не является офертой. Цены действительны на момент совершения рассылки. Актуальные цены уточняйте у менеджера Вашего региона.
+                            </p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+</table>
+<!-- Background grey; -->
+</body>
+
+
+
+
+
+
