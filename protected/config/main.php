98917d2de6e7608f2405e819d15a9547fbbd34a9
Tatsyana Krylova <krilova@lbr.ru>
2015-10-26 15:43:07 +0300

    comment gii

--- protected/config/main.php
+++ protected/config/main.php
@@ -21,12 +21,12 @@ return array(
     ),
     'modules' => array(
         // uncomment the following to enable the Gii tool
-        'gii' => array(
-            'class' => 'system.gii.GiiModule',
-            'password' => 'admin',
-            // If removed, Gii defaults to localhost only. Edit carefully to taste.
-            'ipFilters' => array('127.0.0.1', '::1', '*'),
-        ),
+//        'gii' => array(
+//            'class' => 'system.gii.GiiModule',
+//            'password' => 'admin',
+//            // If removed, Gii defaults to localhost only. Edit carefully to taste.
+//            'ipFilters' => array('127.0.0.1', '::1', '*'),
+//        ),
         'administrator',
         'users',
     ),
