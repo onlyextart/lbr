dfb6d1ff75dc062c125378d06ee35b61c515877c
Vasiliyan Yury <vasiliyan@lbr.ru>
2015-10-26 12:07:40 +0300

    edit contacts

--- protected/views/products/index.php
+++ protected/views/products/index.php
@@ -24,7 +24,7 @@ $productRange = Yii::app()->db->createCommand()
     ->order('title')
     ->queryAll()
 ;
-$link_manager = '/company/contacts/'.Regions::model()->findByPk(Yii::app()->params['regionid'])->contact->id;
+$link_manager = '/company/contacts/'.Regions::model()->findByPk(Yii::app()->params['regionId'])->contact->id;
 if ($data->maker)
 {
     $maker = Makers::model()->findByPk($data->maker);
