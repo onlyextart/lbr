22104ba4f6ee0560759fe799ce1725ff5a4fc8f1
Vasiliyan Yury <vasiliyan@lbr.ru>
2015-11-25 15:38:29 +0300

    comment google script

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -270,9 +270,9 @@
             var google_conversion_label = "3KwoCLnvnwUQn4PvvgM";
             var google_custom_params = window.google_tag_params;
             var google_remarketing_only = true; 
-            /* ]]> */
+            /* ]]>
         </script>-->
-        <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
-        <noscript><div style="display:inline;"><img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/937148831/?value=0&amp;label=3KwoCLnvnwUQn4PvvgM&amp;guid=ON&amp;script=0"/></div></noscript>
+        <!-- <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script> -->
+        <!-- <noscript><div style="display:inline;"><img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/937148831/?value=0&amp;label=3KwoCLnvnwUQn4PvvgM&amp;guid=ON&amp;script=0"/></div></noscript> -->
     </body>
 </html>
