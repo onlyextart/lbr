9eb87fdeeb270d965f0162ee7d64c57b5fa8b96c
Tatsyana Krylova <krilova@lbr.ru>
2015-12-11 12:52:54 +0300

    contacts page

--- protected/controllers/ContactsController.php
+++ protected/controllers/ContactsController.php
@@ -13,16 +13,54 @@ class ContactsController extends Controller
         );
     }
     
-    public function actionIndex() {
+    public function actionIndex() 
+    {
         $contact_id = Yii::app()->params['currentMenuItem']->menuItemsContents[0]->page_id;
-        //var_dump($contact_id);exit;
-        if($contact_id==null){
-            $criteria = new CDbCriteria();
-            $criteria->order = 'name DESC';
-            //$contactModels = Contacts::model()->findAll('published=1');
-            //$this->render('commonContacts', array('contactModels'=>$contactModels));
-            $this->render('commonContacts');
-        } else {
+        
+        if($contact_id == null) { // page with contacts
+            $districts = Regions::getDistrictsForContacst();
+            $output = '<ul class="contacts_list clearfix">
+                <a name="list_filials"></a>'
+            ;
+            
+            foreach($districts as $districtId => $districtName) {
+                $regions = Yii::app()->db->createCommand()
+                    ->selectDistinct('c.name name, c.alias alias, c.address address, c.telephone telephone, c.email email')
+                    ->from('regions r')
+                    ->join('contacts c', 'c.id=r.contact_id')
+                    ->where('r.published=1 AND c.okrug_id=:district_id AND r.district_id=:district_id', array(':district_id'=>$districtId))
+                    ->order('c.name')
+                    ->queryAll()
+                ;
+                
+                if(count($regions) == 1) $output .= '<div class="region-group region-one"><div class="region-group-name">'.$districtName.'</div>';
+                else $output .= '<div class="region-group"><div class="region-group-name">'.$districtName.'</div>';
+                
+                foreach($regions as $region) {
+                    $output .= '<div class="contact-item">';
+                    $output .= '<div class="city-name">'.CHtml::link($region['name'], '/company/contacts/'.$region['alias'].'/').'</div>';
+                    $output .= '<div class="contact-image"><img src="'.Yii::app()->getBaseUrl(true).'/images/ContactsImages/'.$region['alias'].'/main.jpg" /></div>';
+                    $output .= '<div class="contact-info" itemscope="address" itemtype="http://schema.org/LocalBusiness">'.
+                            '<meta itemprop="url" content="www.lbr.ru" />'.
+                            '<meta itemprop="name" content="ЛБР-АгроМаркет" />'.
+                            '<time itemprop="openingHours" datetime="Mo-Su 08:00-17:00" /></time>'.
+                            '<div itemprop="description">'.
+                                '<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">'.
+                                   '<meta itemprop="addressCountry" content="Россия" />'.
+                                   '<div><b>Адрес: </b>'.$region['address'].'</div>'.
+                                '</div>'.
+                            '</div>'.
+                            '<div><b>Телефон: </b>'.$region['telephone'].'</div>'.
+                            '<div itemprop="email"><b>Email: </b>'.$region['email'].'</div>'.
+                          '</div>';
+                    $output .= '</div>';
+                }
+                $output .= '</div>';
+            }
+            $output .= '</ul>';
+            
+            $this->render('commonContacts', array('output'=>$output));
+        } else { // pop-up window for choosing region
             $contactModel = Contacts::model()->findByPk($contact_id);
             $formModel = new ContactForm;
             $this->render('index', array('contactModel'=>$contactModel, 'formModel'=>$formModel));
@@ -31,8 +69,6 @@ class ContactsController extends Controller
     
     public function actionGetRegionsTable()
     {
-//        $regionModel = Regions::model()->findAllByAttributes(array('published'=>'1'), array('order'=>'name ASC'));
         $this->renderPartial('regionstable');
     }
 }
-?>
