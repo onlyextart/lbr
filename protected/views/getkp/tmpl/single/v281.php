c0ab528ddeb16ccf6eca9020de7a196a66f935c1
teterukova <teterukova@lbr.ru>
2015-12-22 15:08:58 +0300

    New Year card for providers

---
+++ protected/views/getkp/tmpl/single/v281.php
@@ -0,0 +1,23 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <style>
+        p{mso-line-height-rule:exactly !important; line-height: 13pt;}
+    </style>
+</head>
+<body style="background: #FFFFFF; margin: 0; padding: 0; border: 0;">
+
+    <style>
+        p{mso-line-height-rule:exactly !important; line-height: 13pt;}
+    </style>
+
+    <table width="800" cellspacing="0" cellpadding="0" align="center" style="border:0; border-collapse: separate;">
+        <tr>
+            <td>
+                <a href="http://www.lbr.ru" target="_blank">
+                    <img src="http://www.lbr.ru/images/kp/281-new-year/card.jpg" width="800" style="border: 0;" alt="Нажмите отобразить рисунки">
+                </a>
+            </td>
+        </tr>
+    </table>
+</body>
+
