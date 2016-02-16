42e459e9c7fe9cbcb8a044b6b1b4d5a59570c3c3
Vasiliyan Yury <vasiliyan@lbr.ru>
2016-02-16 15:28:44 +0300

    kp v297 for e-mail

--- protected/views/getkp/tmpl/single/v297.php
+++ protected/views/getkp/tmpl/single/v297.php
@@ -1,8 +1,148 @@
 <?php
+if ($data['show_header'] == '1' || !isset($data['show_footer'])):
+    Yii::app()->getController()->renderPartial('tmpl/template/mail_header', array('data' => $data, 'filial' => $filial, 'template' => $template));
+endif;
+?>
+<!-- Первый блок -->
+<table width="800" bgcolor="#eeeeee" cellspacing="0" cellpadding="0" align="center" style="border: 0; border-collapse: collapse;">
+    <tr><td style="padding-top: 15px;"><img src="http://www.lbr.ru/images/kp/default/mail-top-line.jpg" width="800" height="10" style="border: 0; float: left;" alt="Картинки не отображаются"></td></tr>
+    <tr>
+        <td>
+            <!-- Внутренности блока: -->
+            <table width="800" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+               
+                <!-- Контент первого блока: -->
+                <tr><td style="padding: 0 20px;">
+                        <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                            <tr>
+                                <td style="padding-top: 3px;">
+                                    <a href="#" style="font-size: 18px; font-weight: 800; font-family: 'Arial Black', Arial; text-decoration: none; color: black;">
+                                        <img src="http://www.lbr.ru/images/kp/297-gvr/v297-1.jpg" width="760" style="border: 0; float: left;" alt="Картинки не отображаются">
+                                    </a>
+                                </td>
+                            </tr>
+                            <?php if ($data['product_info1'] && !empty($data['product_info1'])): ?>
+                                <tr>
+                                    <td style="background: rgb(204, 51, 51); padding: 5px 15px;">
+                                        <p style="margin: 0; color: white; font-family: 'Trebuchet MS', sans-serif; font-size: 15px; font-weight: bold;">
+                                            <?php echo $data['product_info1']; ?>
+                                        </p>
+                                    </td>
+                                </tr>
+                            <?php endif; ?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                            <tr>
+                                <td style="padding: 5px 0px 0px 0px;">
+                                    <table width="760" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="left" style="border: 0; border-collapse: collapse; background: white;">
+                                        <tr>
+                                            <td width="300" valign="top" style="padding: 0px 20px 0px 0px;">
+                                                <table width="350" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+                                                    <tr>
+                                                        <td style="padding: 5px 0px 15px 0px;">
+                                                            <p style="margin: 0px; font-family: 'Trebuchet MS', sans-serif; font-size: 16px; font-weight:normal; text-align:justify; color: black;">
+                                                                <img src="http://www.lbr.ru/images/kp/297-gvr/v297-4.jpg" width="30" style="border: 0; float: left;" alt="Картинки не отображаются">
+                                                                Ворошение прокосов, оборачивание или разбрасывание валков при промокании позволяет ускорить процесс сушки травы и получить высококачественное сено.
+                                                            </p>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td width="350" style="padding: 10px 0px 15px 0px;">
+                                                            <img src="http://www.lbr.ru/images/kp/297-gvr/v297-3.jpg" width="290" style="border: 0; float: left;" alt="Картинки не отображаются">
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                            <td width="280">
+                                                <img src="http://www.lbr.ru/images/kp/297-gvr/v297-2.jpg" width="280" style="border: 0; float: left;" alt="Картинки не отображаются">
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
 
+                            <tr>
+                                <td>
+                                    <table width="700" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center" style="border: 0; border-collapse: collapse; background: white;">
+
+                                        <tr>
+                                            <td style="padding: 5px 20px 0px 20px;">
+                                                <table width="500" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" style="border: 0; border-collapse: collapse; background: #ffffff;">
+                                                    <tr>
+                                                        <td style="padding:0px 0px 0px 0px;">
+                                                            <p style="margin: 0px; padding: 0px 0px 0px 0px; font-family: 'Trebuchet MS', sans-serif; font-size: 18px; text-align:center; color: black;line-height:20px; ">
+                                                                ТЕХНИЧЕСКИЕ ХАРАКТЕРИСТИКИ
+                                                            </p>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        <tr valign="top">
+                                            <td  align="center" style="padding: 0px 20px;">
+                                                <table width="500" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" border="1" bordercolor="#808285" align="center" style="border-collapse: collapse; background: white; border-left: 1px solid grey; border-top: 1px solid grey; border-color:  #808285;">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td width="40%" style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: left; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey; background-clip: padding-box; ">Производительность</td>
+                                                            <td width="12%" style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: center; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey; background-clip: padding-box;">7 га/ч</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: left; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">Скорость рабочая</td>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: center; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">до 12 км/ч</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: left; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">Скорость транспортная</td>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: center; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">20 км/ч</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: left; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">Ширина захвата</td>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: center; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">6000 мм</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: left; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">Ширина валка при сгребании</td>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: center; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">1400 мм</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: left; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">Масса</td>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: center; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">1100 кг</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: left; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">Габариты в рабочем положении</td>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: center; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey;">5000х6300х1600 мм</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: bold; text-align: left; color: black; border-right: 1px solid grey; border-bottom: 1px solid grey; background-clip: padding-box;">Цена с НДС<span style="color:#cc3333; font-size:12px;">*</span></td>
+                                                            <td style="margin: 0; padding: 3px 7px; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: bold; text-align: center; color: #DD2A1B; border-right: 1px solid grey; border-bottom: 1px solid grey; background-clip: padding-box;"><?php echo $data['price1']; ?></td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+
+                                        <tr valign="top">
+                                            <td style="padding: 5px 0px 0px 20px;">
+                                                <p style="margin:0; color:#cc3333; font-weight: bold; font-family: 'Trebuchet MS', sans-serif; font-size: 10px; text-align: center;">
+                                                    * цена сформирована по состоянию на <?php echo date("d.m.Y") ?>
+                                                </p> 
+                                            </td>
+                                        </tr>
+
+
+                                    </table>
+                                </td>
+                            </tr>
+
+                            <!-- Контент первого блока; -->
+                        </table>
+            <!-- Внутренности блока; -->
+        </td>
+    </tr>    
+  </table>
+</td></tr>
+<tr><td><img src="http://www.lbr.ru/images/kp/default/mail-bot-line.jpg" width="800" height="10" style="border: 0; float: left;" alt="Картинки не отображаются"></td></tr>
+</table>
+
+<?php
+if ($data['show_footer'] == '1' || !isset($data['show_footer'])):
+    Yii::app()->getController()->renderPartial('tmpl/template/mail_footer', array('data' => $data, 'filial' => $filial, 'template' => $template));
+endif;
+?>
