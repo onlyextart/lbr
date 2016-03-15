aa4214debbbce6d31e7698b17b0ed5f77c15064b
Tatsyana Krylova <krilova@lbr.ru>
2016-03-15 17:49:53 +0300

    test

--- protected/controllers/ContactsController.php
+++ protected/controllers/ContactsController.php
@@ -82,20 +82,22 @@ class ContactsController extends Controller
     
     public function sendMail($post, $model, $subject)
     {
-        $model->attributes = $post;
-        if ($model->validate()) {
-            $name = '=?UTF-8?B?' . base64_encode($model->name) . '?=';
-            $headers = "From: $name <{$model->email}>\r\n" .
-                    "Reply-To: {$model->email}\r\n" .
-                    "MIME-Version: 1.0\r\n" .
-                    "Content-type: text/plain; charset=UTF-8";
-
-            //mail(Yii::app()->params['adminEmail'], $subject, $model->body, $headers);
-            //mail('krilova@lbr.ru', $subject, $model->body, $headers);
-            mail($model->mailTo, $subject, $model->body, $headers);
-
-            Yii::app()->user->setFlash('success', 'Письмо отправлено. Мы свяжемся с Вами как можно скорее.'.$model->mailTo);
-            $this->refresh();
-        }
+        echo '<pre>';
+        var_dump($model); exit;
+//        $model->attributes = $post;
+//        if ($model->validate()) {
+//            $name = '=?UTF-8?B?' . base64_encode($model->name) . '?=';
+//            $headers = "From: $name <{$model->email}>\r\n" .
+//                    "Reply-To: {$model->email}\r\n" .
+//                    "MIME-Version: 1.0\r\n" .
+//                    "Content-type: text/plain; charset=UTF-8";
+//
+//            //mail(Yii::app()->params['adminEmail'], $subject, $model->body, $headers);
+//            //mail('krilova@lbr.ru', $subject, $model->body, $headers);
+//            mail($model->mailTo, $subject, $model->body, $headers);
+//
+//            Yii::app()->user->setFlash('success', 'Письмо отправлено. Мы свяжемся с Вами как можно скорее.'.$model->mailTo);
+//            $this->refresh();
+//        }
     }
 }
