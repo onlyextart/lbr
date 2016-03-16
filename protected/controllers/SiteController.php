4d47b5464295a982ad31956aa3228a841e943461
Tatsyana Krylova <krilova@lbr.ru>
2016-03-16 11:10:25 +0300

    update

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -11,6 +11,7 @@ class SiteController extends Controller
             'captcha' => array(
                 'class' => 'CCaptchaAction',
                 'backColor' => 0xFFFFFF,
+                'testLimit'=>1,
             ),
             // page action renders "static" pages stored under 'protected/views/site/pages'
             // They can be accessed via: index.php?r=site/page&view=FileName
